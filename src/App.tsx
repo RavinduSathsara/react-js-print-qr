b00c6cd4c22f4fe1ac024f62cde0574398e2f16d
RavinduSathsara <rsathsara330@gmail.com>
2023-03-12 09:00:53 +0530

    clean the code

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,7 @@ import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
 
 function App() {
-  return (
-    <>
-      <Grid
-        container
-        spacing={0}
-        direction="column"
-        alignItems="center"
-        justifyContent="center"
-        style={{ minHeight: "100vh" }}
-      >
-        <Grid item xs={3}>
-          <Button variant="contained">Print QR Label</Button>
-        </Grid>
-      </Grid>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
